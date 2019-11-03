2a3f021c31c9bd3b596be2a22f52d3d4d6c16fe5
nimuc <nimuC@outlook.com>
2019-11-04 00:45:51 +0900

    feat: On join create role

--- lib/personalChannel/personalChannel.go
+++ lib/personalChannel/personalChannel.go
@@ -6,8 +6,23 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+const defaultRoleColor = 0xffffff
+
 func OnJoin(session *discordgo.Session, newMember *discordgo.GuildMemberAdd) {
-	_, err := session.GuildChannelCreate(
+
+	_, err := GuildRoleCreateEdit(
+		session,
+		newMember.GuildID,
+		"ReadWrite:"+newMember.User.ID,
+	)
+
+	_, err = GuildRoleCreateEdit(
+		session,
+		newMember.GuildID,
+		"Read:"+newMember.User.ID,
+	)
+
+	_, err = session.GuildChannelCreate(
 		newMember.GuildID,
 		newMember.User.Username,
 		discordgo.ChannelTypeGuildText,
@@ -16,4 +31,19 @@ func OnJoin(session *discordgo.Session, newMember *discordgo.GuildMemberAdd) {
 		log.Println(err)
 	}
 
+}
+
+//code review: この関数必要ですかね?同じようなことを二回するので、一応分けて書いたのですが...
+func GuildRoleCreateEdit(session *discordgo.Session, guildID, roleName string) (*discordgo.Role, error) {
+	role, err := session.GuildRoleCreate(guildID)
+	if err != nil {
+		return role, err
+	}
+
+	role, err = session.GuildRoleEdit(guildID, role.ID, roleName, defaultRoleColor, false, 0, false)
+	if err != nil {
+		return role, err
+	}
+
+	return role, nil
 }
