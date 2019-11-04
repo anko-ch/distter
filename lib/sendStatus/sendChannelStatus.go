24097af9c575af02757f6fee52a318799cb2b318
nimuC <subaccount@MiyazakinoMacBook-Air.local>
2019-11-04 00:23:18 -0800

    feat: Add channel-permission command

--- lib/sendStatus/sendChannelStatus.go
+++ lib/sendStatus/sendChannelStatus.go
@@ -8,24 +8,45 @@ import (
 )
 
 func OnMessageSendChannelStatus(session *discordgo.Session, message *discordgo.MessageCreate) {
-	if message.Content == "chan-stat" {
+	switch message.Content {
+
+	case "chan-stat":
 		session.ChannelMessageSend(message.ChannelID, channelStatus(session, message))
+
+	case "channel-permission":
+		session.ChannelMessageSend(message.ChannelID, channelPermission(session, message))
+
 	}
 }
 
 func channelStatus(session *discordgo.Session, message *discordgo.MessageCreate) string {
 	var replyMessage string
 
-	channel, err := session.Channel(
-		message.ChannelID,
-	)
+	channel, err := session.Channel(message.ChannelID)
 
 	if err != nil {
 		log.Println(err)
 		replyMessage += err.Error()
 	}
 
-	replyMessage += fmt.Sprintf("```%v```", *channel)
+	replyMessage += fmt.Sprintf("%v", *channel)
 
-	return replyMessage
+	return fmt.Sprintf("```%s```", replyMessage)
+}
+
+func channelPermission(session *discordgo.Session, message *discordgo.MessageCreate) string {
+	var replyMessage string
+
+	channel, err := session.Channel(message.ChannelID)
+
+	if err != nil {
+		log.Println(err)
+		replyMessage += err.Error()
+	}
+
+	for _, permissionOverWrite := range channel.PermissionOverwrites {
+		replyMessage += fmt.Sprintf("%v", permissionOverWrite)
+	}
+
+	return fmt.Sprintf("```%s```", replyMessage)
 }
